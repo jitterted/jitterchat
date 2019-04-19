69f18eb46cc0c219b04903497c1012ef71a0ed91
Ted M. Young <tedyoung@gmail.com>
2019-04-19 15:32:57 -0700

    Episode 3/4 of JitterChat: * New chat text HTML-styled pane * Added notification for channel leave events

--- src/main/java/com/jitterted/ChatToolWindow.java
+++ src/main/java/com/jitterted/ChatToolWindow.java
@@ -1,11 +1,13 @@
 package com.jitterted;
 
 import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.philippheuer.events4j.EventManager;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.TwitchChat;
 import com.github.twitch4j.chat.events.CommandEvent;
 import com.github.twitch4j.chat.events.channel.ChannelJoinEvent;
+import com.github.twitch4j.chat.events.channel.ChannelLeaveEvent;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
@@ -13,17 +15,19 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
-import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Element;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 import java.awt.event.ActionEvent;
 import java.io.FileReader;
 import java.io.IOException;
@@ -43,53 +47,14 @@ public class ChatToolWindow {
   private final Project project;
   private JButton connectButton;
   private JPanel chatContentPanel;
-  private JTextArea chatText;
+  private JTextPane chatTextPane;
   private JScrollPane chatScrollPane;
   private JTextField myMessage;
-  private JLabel editorCommentCountLabel;
 
   private String oAuthToken;
   private String twitchClientId;
   private String twitchClientSecret;
-
-  public ChatToolWindow(ToolWindow toolWindow, Project project) {
-    this.project = project;
-    Properties twitchProperties = new Properties();
-    try {
-      twitchProperties.load(new FileReader("/Users/ted/.twitch.properties"));
-      oAuthToken = twitchProperties.getProperty(TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY);
-      twitchClientId = twitchProperties.getProperty(TWITCH_API_CLIENT_ID_PROPERTY_KEY);
-      twitchClientSecret = twitchProperties.getProperty(TWITCH_API_CLIENT_SECRET_PROPERTY_KEY);
-
-      chatText.setText("Twitch properties loaded.\n\n");
-
-      myMessage.addActionListener(this::sendMessage);
-
-      connectButton.addActionListener(e -> connect());
-
-    } catch (IOException e) {
-      chatText.setText("Unable to load Twitch Properties: " + e.getMessage());
-      connectButton.setEnabled(false);
-    }
-
-    project.getMessageBus().connect().subscribe(
-        FileEditorManagerListener.FILE_EDITOR_MANAGER,
-        new FileEditorManagerListener() {
-          @Override
-          public void selectionChanged(@NotNull FileEditorManagerEvent event) {
-            VirtualFile newFile = event.getNewFile();
-            String commentCount;
-            if (newFile == null) {
-              commentCount = "<no file>";
-            } else {
-              ChatCommentLookup service = ServiceManager.getService(ChatCommentLookup.class);
-              commentCount = newFile.getNameWithoutExtension() + ": " + service.commentCountFor(newFile);
-            }
-            ApplicationManager.getApplication().invokeLater(() -> editorCommentCountLabel.setText(commentCount));
-          }
-        });
-
-  }
+  private HTMLDocument htmlDocument;
 
   private Editor currentEditor() {
     return FileEditorManager.getInstance(project).getSelectedTextEditor();
@@ -107,6 +72,30 @@ public class ChatToolWindow {
 
     textField.setText("");
   }
+  private Element chatElement;
+
+  public ChatToolWindow(ToolWindow toolWindow, Project project) {
+    initializeTextPane();
+    this.project = project;
+    Properties twitchProperties = new Properties();
+    try {
+      twitchProperties.load(new FileReader("/Users/ted/.twitch.properties"));
+      oAuthToken = twitchProperties.getProperty(TWITCH_API_OAUTH_ACCESS_TOKEN_PROPERTY_KEY);
+      twitchClientId = twitchProperties.getProperty(TWITCH_API_CLIENT_ID_PROPERTY_KEY);
+      twitchClientSecret = twitchProperties.getProperty(TWITCH_API_CLIENT_SECRET_PROPERTY_KEY);
+
+      addItalicized("Twitch properties loaded.\n\n");
+
+      myMessage.addActionListener(this::sendMessage);
+
+      connectButton.addActionListener(e -> connect());
+
+    } catch (IOException e) {
+      addItalicized("Unable to load Twitch Properties: " + e.getMessage());
+      connectButton.setEnabled(false);
+    }
+
+  }
 
   private void connect() {
     OAuth2Credential credential = new OAuth2Credential("twitch", oAuthToken);
@@ -122,23 +111,32 @@ public class ChatToolWindow {
 
     chat.joinChannel("jitterted");
     chat.sendMessage("jitterted", "The JitterChat remote control bot is here!");
-    chat.getEventManager()
+
+    EventManager chatEventManager = chat.getEventManager();
+
+    chatEventManager
         .onEvent(ChannelMessageEvent.class)
         .subscribe(this::onChannelMessage);
-    chat.getEventManager()
+
+    chatEventManager
         .onEvent(CommandEvent.class)
         .subscribe(this::onCommand);
-    chat.getEventManager()
-        .onEvent(ChannelJoinEvent.class)
-        .subscribe(this::onJoin);
 
-    chatText.setText(">> JitterChat is connected.\n");
+    chatEventManager
+        .onEvent(ChannelLeaveEvent.class)
+        .subscribe(this::onLeave);
+
+     chatEventManager
+         .onEvent(ChannelJoinEvent.class)
+         .subscribe(this::onJoin);
+
+    addItalicized("JitterChat is connected.");
     connectButton.setText("Disconnect from Twitch");
     connectButton.setEnabled(false);
   }
 
-  private void onJoin(ChannelJoinEvent channelJoinEvent) {
-    chatText.append(">> " + channelJoinEvent.getUser().getName() + " has joined.\n");
+  private void onLeave(ChannelLeaveEvent channelLeaveEvent) {
+    addItalicized("<small>" + channelLeaveEvent.getUser().getName() + " has left.</small>");
   }
 
   private void onCommand(CommandEvent commandEvent) {
@@ -203,6 +201,17 @@ public class ChatToolWindow {
     selectedTextEditor.getCaretModel().moveToLogicalPosition(new LogicalPosition(lineNumber, 0));
   }
 
+  private void onJoin(ChannelJoinEvent channelJoinEvent) {
+    addItalicized("<small>" + channelJoinEvent.getUser().getName() + " has joined.</small>");
+  }
+
+  private void addItalicized(String text) {
+    addContentInDiv("<i>" + text + "</i>");
+  }
+
+  public JPanel getContent() {
+    return chatContentPanel;
+  }
 
   private void onChannelMessage(ChannelMessageEvent event) {
     Instant eventFiredAt = event.getFiredAt().toInstant();
@@ -211,14 +220,38 @@ public class ChatToolWindow {
 
     String time = CHAT_TIME_FORMATTER.format(eventFiredAt.atZone(Clock.systemDefaultZone().getZone()));
 
-    chatWrite("[" + time + "] " + userName + ": " + message);
+    chatWrite("<small>[" + time + "]</small> <b>" + userName + "</b>: " + message);
   }
 
   private void chatWrite(String message) {
-    chatText.append(message + "\n");
+    addHtml("<p>" + message + "</p>");
   }
 
-  public JPanel getContent() {
-    return chatContentPanel;
+  private void initializeTextPane() {
+    StyleSheet styleSheet = new StyleSheet();
+    styleSheet.addRule("body { font-face: sans-serif; }");
+
+    HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+    htmlEditorKit.setStyleSheet(styleSheet);
+    htmlDocument = (HTMLDocument) htmlEditorKit.createDefaultDocument();
+    chatTextPane.setEditorKit(htmlEditorKit);
+    chatTextPane.setDocument(htmlDocument);
+    chatTextPane.setText("<html><head></head><body><div id='chat'></div></body></html>");
+
+    chatElement = htmlDocument.getElement("chat");
+
+    addContentInDiv("Ready to go...");
+  }
+
+  private void addContentInDiv(String text) {
+    addHtml("<div>" + text + "</div>");
+  }
+
+  private void addHtml(String content)  {
+    try {
+      htmlDocument.insertBeforeEnd(chatElement, content);
+    } catch (BadLocationException | IOException e) {
+      e.printStackTrace();
+    }
   }
 }
